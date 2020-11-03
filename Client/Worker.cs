a0cb9a92ca2651585135a76f8f96d4abb31e2648
xxx-PC <545576260@qq.com>
2020-11-03 18:02:23 +0800

    完善获取有效远程ip方法

--- Client/Worker.cs
+++ Client/Worker.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -76,6 +78,54 @@ namespace FinalController
             }, stoppingToken);
         }
 
+
+        //获取远程ip集
+        private List<string> getRemoteAddress()
+        {
+            string[] address = getLocalAddress();
+            if (address.Length==0)
+                return null;
+
+            List<string> ips = new List<string>();
+            
+            foreach (string _address in address)
+            {
+                string ipHead = _address.Remove(_address.LastIndexOf('.') + 1);
+                int ipEnd =  Int32.Parse(_address.Remove(0, _address.Length));
+                for (int i = 1; i <= 255; i++)
+                {
+                    if(i==ipEnd)
+                        continue;
+
+                    string ip = string.Format("{0}.{1}", _address, ipEnd);
+                    if (PingIp(ip))
+                    {
+                        ips.Add(ip);
+                    }
+                }
+            }
+
+            return ips;
+        }
+
+        //获取本地Ip集
+        private string[] getLocalAddress()
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(hostName);
+            
+            string[] ips = new string[ipHostEntry.AddressList.Length];
+            for (var i = 0; i < ips.Length; i++)
+            {
+                IPAddress curIp = ipHostEntry.AddressList[i];
+                if (curIp.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ips[i] = curIp.ToString();
+                }
+            }
+            return ips;
+        }
+        
         private bool PingIp(string strIP)
         {
             bool bRet = false;
@@ -94,6 +144,6 @@ namespace FinalController
 
             return bRet;
         }
-
+    
     }
 }
